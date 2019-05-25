cf196ea7272d83ee6088b258c94c4be21a5eff67
Calvin Moore <c1moore@outlook.com>
2019-05-25 10:26:55 -0400

    Coded service for fetching all guesses by a user.

---
+++ services/list-user-guesses.ts
@@ -0,0 +1,45 @@
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+  region: process.env.AWS_REGION,
+});
+
+exports.fetchGuessesByUser = async ({ pathParameters = {} }: { pathParameters: { guessor?: string } }, _context: any, callback: (Error, any) => void) => {
+  try {
+    const guessor = decodeURIComponent(pathParameters.guessor);
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    const guessorDoc = (await docClient.get({
+      TableName:  process.env.DYNAMO_TABLE_NAME,
+      Key:        {
+        Guessor:    guessor,
+      },
+    }).promise());
+
+    if (!guessorDoc || !guessorDoc.Item) {
+      callback(null, {
+        statusCode: 404,
+      });
+
+      return;
+    }
+
+    callback(null, {
+      statusCode: 200,
+      headers:    {
+        'Content-Type': 'application/json',
+      },
+      body:       JSON.stringify(guessorDoc.Item.guesses),
+    });
+  } catch (err) {
+    callback(null, {
+      statusCode: 500,
+      headers:    {
+        'Content-Type': 'application/json',
+      },
+      body:       JSON.stringify({
+        msg:  err.toString(),
+      }),
+    });
+  }
+};
